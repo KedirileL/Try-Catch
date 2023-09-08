b27abe748eae6787b4b6ea0e3ed736907447a027
user <user@LAPTOP-KAN0NU4I>
2023-09-08 22:38:33 +0200

    Add project files.

---
+++ Try Catch/Form1.Designer.vb
@@ -0,0 +1,75 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()>
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()>
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()>
+    Private Sub InitializeComponent()
+        Button1 = New Button()
+        Label1 = New Label()
+        txtNumber = New TextBox()
+        SuspendLayout()
+        ' 
+        ' Button1
+        ' 
+        Button1.BackColor = SystemColors.ActiveCaption
+        Button1.Location = New Point(322, 283)
+        Button1.Name = "Button1"
+        Button1.Size = New Size(112, 34)
+        Button1.TabIndex = 0
+        Button1.Text = "Output"
+        Button1.UseVisualStyleBackColor = False
+        ' 
+        ' Label1
+        ' 
+        Label1.AutoSize = True
+        Label1.Location = New Point(126, 95)
+        Label1.Name = "Label1"
+        Label1.Size = New Size(119, 25)
+        Label1.TabIndex = 1
+        Label1.Text = "Enter number"
+        ' 
+        ' txtNumber
+        ' 
+        txtNumber.Location = New Point(354, 89)
+        txtNumber.Name = "txtNumber"
+        txtNumber.Size = New Size(150, 31)
+        txtNumber.TabIndex = 2
+        ' 
+        ' Form1
+        ' 
+        AutoScaleDimensions = New SizeF(10F, 25F)
+        AutoScaleMode = AutoScaleMode.Font
+        BackColor = SystemColors.ActiveBorder
+        ClientSize = New Size(800, 450)
+        Controls.Add(txtNumber)
+        Controls.Add(Label1)
+        Controls.Add(Button1)
+        ForeColor = SystemColors.ActiveCaptionText
+        Name = "Form1"
+        Text = "Form1"
+        ResumeLayout(False)
+        PerformLayout()
+    End Sub
+
+    Friend WithEvents Button1 As Button
+    Friend WithEvents Label1 As Label
+    Friend WithEvents txtNumber As TextBox
+End Class
---
+++ Try Catch/Form1.vb
@@ -0,0 +1,20 @@
+﻿Public Class Form1
+    Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
+
+        Dim Number As String
+        Number = txtNumber.Text
+
+        Try
+            Dim numericValue As Double
+            numericValue = CDbl(Number)
+
+            MsgBox("You Entered a valid number: " & numericValue)
+
+        Catch ex As Exception
+
+            MsgBox("Error: " & Number & " is not a number ")
+
+        End Try
+
+    End Sub
+End Class
